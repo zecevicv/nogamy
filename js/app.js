72dc5f23064c93d459562698494a6c85a45194f4
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-08-17 11:32:43 +0200

    [ADD] - Video Modal

--- js/app.js
+++ js/app.js
@@ -57,3 +57,34 @@ if (document.querySelector('.reviews .swiper-container')) {
     }
   });
 }
+
+/* #Video Modal
+  ======================================================= */
+const body = document.querySelector('body');
+const videoModalToggler = document.querySelector('.home-banner .video');
+
+// Message Modal Handler
+if (videoModalToggler) {
+  videoModalToggler.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modal = document.querySelector(videoModalToggler.dataset.modal);
+
+    if (modal) {
+      modal.classList.toggle('show');
+      body.classList.toggle('no-scroll');
+    }
+  });
+}
+
+// Modal Close
+const videoModal = document.querySelector('.video-modal');
+
+if (videoModal) {
+  videoModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.closest('.modal-close')) {
+      videoModal.classList.toggle('show');
+      body.classList.toggle('no-scroll');
+    }
+  });
+}
